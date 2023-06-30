2ee6f395380114be3eed7613ceae80143e4dbb20
Kamok1 <kkamiss12@gmail.com>
2023-06-30 15:47:15 +0200

    remove unused function

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -1,6 +1,4 @@
-﻿using System.Globalization;
-
-namespace Helpers;
+﻿namespace Helpers;
 
 public static class Helpers
 {
@@ -12,12 +10,5 @@ public static class Helpers
             list.Add(item.Key + "=" + item.Value);
         }
         return string.Join("&", list);
-    }
-
-    public static DateTimeOffset DateTimeOffsetFromISO8601(this string date)
-    {
-        string format = "yyyyMMdd'T'HHmmss.fff'Z'";
-        DateTime dateTime = DateTime.ParseExact(date, format, CultureInfo.InvariantCulture);
-        return dateTime;
     }
 }
